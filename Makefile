a0011929b9ddbf24b91eb5a7c7b9d311bb26ad2e
jana-sw <74660676+jana-sw@users.noreply.github.com>
2021-02-26 19:23:45 +0100

    wprodzadzenie pliku testowego

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 automat: main.c matrix-io.c
-	gcc -g -o automat main.c matrix-io.c
+	gcc -Wall -pedantic -g -o automat main.c matrix-io.c
+
+test1: automat 
+	test 2
--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "matrix-io.h"
 
 int main(int argc, char **argv) {
-
+	
+	if(argc < 2) {
+		printf("Błędna liczba podawanych argumentów");
+		return 1;
+	}
+	Matrix *mat = readFromFile(argv[1]);
+	
+	
+	return 0;
 
 }
