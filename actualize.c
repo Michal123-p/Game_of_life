92353074cc6c714d6594823bc36125e5d77f352c
123Michal-p <m.bilinskimichal@gmail.com>
2021-03-04 12:37:43 +0100

    Poprawione liczenie komórek

--- actualize.c
+++ actualize.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "actualize.h"
+#include <stdio.h>
 
 /* stany:
  0 - martwa
@@ -12,9 +13,9 @@ int countAliveCells (Mat *mat, int x, int y ) {  //x i y to współrzędne punkt
 	int sumAlive = 0;
 	for (int i = x - 1; i < x + 2; i++) // iteracja po wierszach w sąsiedstwie
 		for (int j = y - 1; j < y + 2; j++) // iteracja po kolumnach w sąsiedstwie
-			if ( (i >= 0 && i < mat->row) && (j >= 0 && j < mat->col) && (i!= x && j!= y) )
-				if(mat->data[i][j] == 1 || mat->data[i][j] == 3 )   //króra w tym momencie jest żywa
-					sumAlive++;
+			if ( (i >= 0 && i < mat->row) && (j >= 0 && j < mat->col) && (i!= x || j!= y) ) {
+				if(mat->data[i][j] == 1 || mat->data[i][j] == 3 )   //króra  tym momencie jest żywa
+					sumAlive++;}
 	return sumAlive;
 }
 
@@ -23,9 +24,10 @@ Mat* updateMatrix (Mat *mat) {
     for (int i = 0; i < mat->row; i++)
         for (int j = 0; j < mat->col; j++) {
             sumAlive = countAliveCells(mat, i, j);
-            if (mat->data[i][j] == 1 && (sumAlive != 2 && sumAlive != 3))
+            if (mat->data[i][j] == 1 && (sumAlive != 2 && sumAlive != 3)) 
                 mat->data[i][j] = 3;  //ginie
-            if (mat->data[i][j] == 0 && sumAlive == 3)
+	   
+	    if (mat->data[i][j] == 0 && sumAlive == 3)
                 mat->data[i][j] = 2;   //rodzi się
             }
     mat = fixMatrix(mat);
